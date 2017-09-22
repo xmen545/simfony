26ad9b1a9684bba22f264122a341271a368560ca
AndreyShevchuk <office@shevchuk-studio.pw>
2017-09-22 21:51:00 +0000

    HomeWork Routing

--- my_project_name/src/AppBundle/Controller/DefaultController.php
+++ my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,39 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    public function newsAction($lang){
+        
+        die('News Action is work! My lang is: <b>'.$lang.'</b>');
+    }
+    public function newsWithLangAction($lang){
+        
+        die('News Action is work! My lang is:  <b>'.$lang.'</b>');
+    }
+    public function articleGetAction($articlename){ 
+    
+        die('Articke alias Action is work! Article Alias is: <b>'.$articlename.'<b>');
+    } 
+    public function newsPagesListAction($page){ 
+    
+        die('Pagination Action is work! Page is: <b>'.$page.'<b>');
+    }
+    public function newsSortAction($page, $title, $desc){ 
+       switch($desc){
+            case'default':
+                print('Parameter Desc should not be empty!'.PHP_EOL);
+            break;
+            
+        }switch($title){
+            case'default':
+                print('Parameter Title should not be empty!'.PHP_EOL);
+            break;
+            
+        }switch($page){
+            case'default':
+                print('Parameter Page should not be empty!'.PHP_EOL);
+            break;
+            
+        }
+        die('News sort Action is work! Sort parameters: <b>'.$page.', '.$title.', '.$desc.'<b>');
+    }
 }
--- my_project_name/web/app.php
+++ my_project_name/web/app.php
@@ -1,20 +1,23 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
-if (PHP_VERSION_ID < 70000) {
-    include_once __DIR__.'/../var/bootstrap.php.cache';
-}
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
+//umask(0000);
 
-$kernel = new AppKernel('prod', false);
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+
+
+require __DIR__.'/../vendor/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
